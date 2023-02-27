ef6d582c13bfd0fc9f5c76a18f504748f441b772
RadmiloM <markovicradmilo35@gmail.com>
2023-02-27 11:38:41 +0100

    UserService method getUserById created and method createUser changed as registerUser

--- src/main/java/com/rentacar/RentACar/service/UserService.java
+++ src/main/java/com/rentacar/RentACar/service/UserService.java
@@ -2,9 +2,12 @@ package com.rentacar.RentACar.service;
 
 import com.rentacar.RentACar.entity.User;
 import com.rentacar.RentACar.exception.UsernameAlreadyExistsException;
+import com.rentacar.RentACar.exception.UsernameNotFoundException;
 import com.rentacar.RentACar.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +15,19 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public void createUser(User user){
+    public void registerUser(User user){
         var userDB = userRepository.findByUsername(user.getUsername());
         if(userDB.isPresent()){
             throw new UsernameAlreadyExistsException("user with username " + user.getUsername() + " already exists");
         }
         userRepository.save(user);
+    }
+
+    public User getUserById(UUID uuid){
+        var userDB = userRepository.findById(uuid);
+        if(userDB.isEmpty()){
+            throw new UsernameNotFoundException("User with id: " + uuid + " does not exists in database");
+        }
+        return userDB.get();
     }
 }
