342050ed817cbdab7e0f955b6639eea8dcdfb152
RadmiloM <markovicradmilo35@gmail.com>
2023-02-27 09:42:28 +0100

    UserRequest and UserResponse classes for data object transfer created.

---
+++ src/main/java/com/rentacar/RentACar/dto/UserRequest.java
@@ -0,0 +1,13 @@
+package com.rentacar.RentACar.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRequest {
+
+    private String username;
+    private String email;
+    private String password;
+
+
+}
---
+++ src/main/java/com/rentacar/RentACar/dto/UserResponse.java
@@ -0,0 +1,10 @@
+package com.rentacar.RentACar.dto;
+
+import lombok.Data;
+
+@Data
+public class UserResponse {
+
+    private boolean successful;
+    private String message;
+}
