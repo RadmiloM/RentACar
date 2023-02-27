f61f74ed8119b35cdd916eba1adba3cbc74842c0
RadmiloM <markovicradmilo35@gmail.com>
2023-02-27 12:26:26 +0100

    userupdate request mapping created.

--- src/main/java/com/rentacar/RentACar/mapper/UserMapper.java
+++ src/main/java/com/rentacar/RentACar/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.rentacar.RentACar.mapper;
 
 import com.rentacar.RentACar.dto.UserRequest;
+import com.rentacar.RentACar.dto.UserUpdateRequest;
 import com.rentacar.RentACar.entity.User;
 import org.springframework.stereotype.Component;
 
@@ -16,4 +17,17 @@ public class UserMapper {
         return user;
 
     }
+
+    public User mapToEntity(UserUpdateRequest userUpdateRequest){
+        User user = new User();
+        user.setUsername(userUpdateRequest.getUsername());
+        user.setPassword(userUpdateRequest.getPassword());
+        user.setFirstName(userUpdateRequest.getFirstName());
+        user.setLastName(userUpdateRequest.getLastName());
+        user.setPhoneNumber(userUpdateRequest.getPhoneNumber());
+        user.setImage(userUpdateRequest.getImage());
+        return user;
+
+    }
+
 }
