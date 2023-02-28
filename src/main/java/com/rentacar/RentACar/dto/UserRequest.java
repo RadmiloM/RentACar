ad9252c87dc29861f034b9e15f6577bd29f27863
RadmiloM <markovicradmilo35@gmail.com>
2023-02-28 19:05:20 +0100

    unnecessary import deleted.

--- src/main/java/com/rentacar/RentACar/dto/UserRequest.java
+++ src/main/java/com/rentacar/RentACar/dto/UserRequest.java
@@ -1,7 +1,6 @@
 package com.rentacar.RentACar.dto;
 
 import com.rentacar.RentACar.validation.ValidPassword;
-import com.sun.istack.NotNull;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
@@ -12,7 +11,7 @@ import javax.validation.constraints.Size;
 @Data
 public class UserRequest {
     @NotEmpty(message = "username may not be empty or null")
-    @Size(min=3,message = "username must be at least 3 characters long")
+    @Size(min = 3, message = "username must be at least 3 characters long")
     private String username;
     @NotEmpty(message = "email may not be empty or null")
     @Email
