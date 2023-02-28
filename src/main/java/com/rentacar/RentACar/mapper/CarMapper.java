78b301b09a4ea889b017fb4cd00cf815021785fc
RadmiloM <markovicradmilo35@gmail.com>
2023-02-28 14:57:20 +0100

    map to dto methods created.

--- src/main/java/com/rentacar/RentACar/mapper/CarMapper.java
+++ src/main/java/com/rentacar/RentACar/mapper/CarMapper.java
@@ -1,8 +1,12 @@
 package com.rentacar.RentACar.mapper;
 
 import com.rentacar.RentACar.dto.CarRequest;
+import com.rentacar.RentACar.dto.CarResponse;
 import com.rentacar.RentACar.entity.Car;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CarMapper {
@@ -23,5 +27,27 @@ public class CarMapper {
         car.setFuel(carRequest.getFuel());
         car.setImage(carRequest.getImage());
         return car;
+    }
+
+    public CarResponse mapToDTO(Car car){
+        CarResponse carResponse = new CarResponse();
+        carResponse.setLicencePlate(car.getLicencePlate());
+        carResponse.setMake(car.getMake());
+        carResponse.setModel(car.getModel());
+        carResponse.setYear(car.getYear());
+        carResponse.setEngineCapacity(car.getEngineCapacity());
+        carResponse.setColor(car.getColor());
+        carResponse.setPrice(car.getPrice());
+        carResponse.setDoors(car.getDoors());
+        carResponse.setSize(car.getSize());
+        carResponse.setPower(car.getPower());
+        carResponse.setAutomatic(car.isAutomatic());
+        carResponse.setFuel(car.getFuel());
+        carResponse.setImage(car.getImage());
+        return carResponse;
+    }
+
+    public List<CarResponse> mapToDTO(List<Car> cars){
+        return cars.stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 }
