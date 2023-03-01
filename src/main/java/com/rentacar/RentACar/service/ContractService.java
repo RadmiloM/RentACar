2b469071c15156a6aed995fc1accd21e42e59360
RadmiloM <markovicradmilo35@gmail.com>
2023-03-01 17:27:05 +0100

    contract service created.

---
+++ src/main/java/com/rentacar/RentACar/service/ContractService.java
@@ -0,0 +1,23 @@
+package com.rentacar.RentACar.service;
+
+import com.rentacar.RentACar.entity.Car;
+import com.rentacar.RentACar.entity.Contract;
+import com.rentacar.RentACar.entity.User;
+import com.rentacar.RentACar.repository.ContractRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ContractService {
+
+    private final ContractRepository contractRepository;
+
+    public void createContract(User user, Contract contract, Car car) {
+        contract.setUser(user);
+        contract.setCar(car);
+        contractRepository.save(contract);
+    }
+
+
+}
