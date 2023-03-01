e52025f786677a5026ea0acea7e8eb0dd95ecfd0
RadmiloM <markovicradmilo35@gmail.com>
2023-03-01 20:00:01 +0100

    controller methods created.

--- src/main/java/com/rentacar/RentACar/controller/ContractController.java
+++ src/main/java/com/rentacar/RentACar/controller/ContractController.java
@@ -1,20 +1,15 @@
 package com.rentacar.RentACar.controller;
 
-import com.rentacar.RentACar.dto.ContractRequest;
-import com.rentacar.RentACar.dto.ContractResponse;
-import com.rentacar.RentACar.dto.ContractShortResponse;
-import com.rentacar.RentACar.entity.User;
+import com.rentacar.RentACar.dto.*;
 import com.rentacar.RentACar.mapper.ContractMapper;
 import com.rentacar.RentACar.service.CarService;
 import com.rentacar.RentACar.service.ContractService;
 import com.rentacar.RentACar.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -30,30 +25,56 @@ public class ContractController {
 
 
     @PostMapping("/contracts")
-    public ResponseEntity<ContractShortResponse> createContract(@RequestBody ContractRequest contractRequest){
-            var contract = contractMapper.mapToEntity(contractRequest);
-            var user = userService.getUserById(contractRequest.getUserId());
-            var car = carService.getCarById(contractRequest.getCarId());
-            contractService.createContract(user,contract,car);
-            ContractShortResponse contractShortResponse = new ContractShortResponse();
-            contractShortResponse.setMessage("Ugovor kreiran, ceka odobrenje");
-            contractShortResponse.setSuccessful(true);
-            return ResponseEntity.ok(contractShortResponse);
+    public ResponseEntity<ContractShortResponse> createContract(@RequestBody ContractRequest contractRequest) {
+        var contract = contractMapper.mapToEntity(contractRequest);
+        var user = userService.getUserById(contractRequest.getUserId());
+        var car = carService.getCarById(contractRequest.getCarId());
+        contractService.updateContract(user, contract, car);
+        ContractShortResponse contractShortResponse = new ContractShortResponse();
+        contractShortResponse.setMessage("Ugovor kreiran, ceka odobrenje");
+        contractShortResponse.setSuccessful(true);
+        return ResponseEntity.ok(contractShortResponse);
     }
 
-//    @PostMapping("/contracts/sample")
-//    public ResponseEntity<ContractResponse> createSample(@RequestBody ContractRequest contractRequest){
-//        var contract = contractMapper.mapToEntity(contractRequest);
-//        contractService.createContract(contract);
-//        var contractResponse = ContractResponse
-//                .builder()
-//                .userId(contractRequest.getUserId())
-//                .carId(contractRequest.getCarId())
-//                .startDate(contractRequest.getStartDate())
-//                .endDate(contractRequest.getEndDate())
-//                .totalPrice(550.99)
-//                .signed(false)
-//                .build();
-//        return ResponseEntity.ok(contractResponse);
-//    }
+    @GetMapping("/contracts")
+    public ResponseEntity<List<FullContractResponse>> fetchAllContracts(@RequestHeader("id") UUID uuid) {
+        var contracts = contractService.fetchAllContracts();
+        var fullContractResponse = contractMapper.mapToDTO(contracts);
+        return ResponseEntity.ok(fullContractResponse);
+    }
+
+    @GetMapping("/contracts/pending")
+    public ResponseEntity<List<FullContractResponse>> fetchPendingContracts(@RequestHeader("id") UUID uuid) {
+        var contracts = contractService.findPendingContracts();
+        var fullContractResponse = contractMapper.mapToDTO(contracts);
+        return ResponseEntity.ok(fullContractResponse);
+    }
+
+    @PostMapping("/contracts/{contractId}/approval")
+    public ResponseEntity<Void> approveContract(@RequestHeader("id") UUID uuid,
+                                                @PathVariable("contractId") UUID contractId,
+                                                @RequestBody ShortContractRequest shortContractRequest){
+        var contract = contractService.getContractById(contractId);
+        if(!shortContractRequest.isApproved()){
+            contract.setApproved(true);
+            contractService.updateContract(contract);
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/contracts/{userId}/history")
+    public ResponseEntity<List<FullContractResponse>> fetchUserContractsHistory(@PathVariable("userId") UUID uuid){
+        var user = userService.getUserById(uuid);
+        var contracts = contractService.getAllContractsByUserId(user);
+        var response = contractMapper.mapToDTO(contracts);
+        return ResponseEntity.ok(response);
+
+    }
+
+    @PostMapping("/contracts/sample")
+    public ResponseEntity<List<FullContractResponse>> createSample(@RequestBody ContractRequest contractRequest){
+        var contract = contractService.findByDates(contractRequest.getStartDate(),contractRequest.getEndDate());
+        var contractResponse = contractMapper.mapToDTO(contract);
+        return ResponseEntity.ok(contractResponse);
+    }
 }
