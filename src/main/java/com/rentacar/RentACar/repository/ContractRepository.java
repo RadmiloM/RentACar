544fe65f572d449a0f7a7f00a7e656a9cde902f9
RadmiloM <markovicradmilo35@gmail.com>
2023-03-01 18:11:47 +0100

    method find by approved created.

--- src/main/java/com/rentacar/RentACar/repository/ContractRepository.java
+++ src/main/java/com/rentacar/RentACar/repository/ContractRepository.java
@@ -4,8 +4,12 @@ import com.rentacar.RentACar.entity.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, UUID> {
+
+    List<Contract> findByApprovedFalse();
+
 }
