2f4651300ae825d749a501e607971d4f2e56e99a
RadmiloM <markovicradmilo35@gmail.com>
2023-03-01 18:43:50 +0100

    update contract method created.

--- src/main/java/com/rentacar/RentACar/service/ContractService.java
+++ src/main/java/com/rentacar/RentACar/service/ContractService.java
@@ -3,11 +3,13 @@ package com.rentacar.RentACar.service;
 import com.rentacar.RentACar.entity.Car;
 import com.rentacar.RentACar.entity.Contract;
 import com.rentacar.RentACar.entity.User;
+import com.rentacar.RentACar.exception.ContractNotFoundException;
 import com.rentacar.RentACar.repository.ContractRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +17,7 @@ public class ContractService {
 
     private final ContractRepository contractRepository;
 
-    public void createContract(User user, Contract contract, Car car) {
+    public void updateContract(User user, Contract contract, Car car) {
         contract.setUser(user);
         contract.setCar(car);
         contractRepository.save(contract);
@@ -28,6 +30,20 @@ public class ContractService {
     public List<Contract> findPendingContracts(){
         return contractRepository.findByApprovedFalse();
     }
+
+    public Contract getContractById(UUID uuid){
+        var contract = contractRepository.findById(uuid);
+        if(contract.isEmpty()){
+            throw new ContractNotFoundException("contract with id: " + uuid + " does not exists in database");
+        }
+        return contract.get();
+    }
+
+    public void updateContract(Contract contract){
+        contractRepository.save(contract);
+    }
+
+
 
 
 }
