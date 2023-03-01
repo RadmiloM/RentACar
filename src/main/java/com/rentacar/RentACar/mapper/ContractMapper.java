c690a206e5d99de5401a4d727f8609d1f3784310
RadmiloM <markovicradmilo35@gmail.com>
2023-03-01 17:51:01 +0100

    new method for mapping created.

--- src/main/java/com/rentacar/RentACar/mapper/ContractMapper.java
+++ src/main/java/com/rentacar/RentACar/mapper/ContractMapper.java
@@ -1,8 +1,13 @@
 package com.rentacar.RentACar.mapper;
 
 import com.rentacar.RentACar.dto.ContractRequest;
+import com.rentacar.RentACar.dto.ContractResponse;
+import com.rentacar.RentACar.dto.FullContractResponse;
 import com.rentacar.RentACar.entity.Contract;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ContractMapper {
@@ -15,5 +20,22 @@ public class ContractMapper {
         contract.setTotalPrice(contractRequest.getTotalPrice());
         contract.setSigned(contractRequest.isSigned());
         return contract;
+    }
+
+    public FullContractResponse mapToDTO(Contract contract){
+        FullContractResponse fullContractResponse = new FullContractResponse();
+        fullContractResponse.setId(contract.getId());
+        fullContractResponse.setUserId(contract.getUser().getId());
+        fullContractResponse.setCarId(contract.getCar().getId());
+        fullContractResponse.setStartDate(contract.getStartDate());
+        fullContractResponse.setEndDate(contract.getEndDate());
+        fullContractResponse.setTotalPrice(contract.getTotalPrice());
+        fullContractResponse.setSigned(contract.isSigned());
+        fullContractResponse.setApproved(contract.isApproved());
+        return fullContractResponse;
+    }
+
+    public List<FullContractResponse> mapToDTO(List<Contract> contracts){
+        return contracts.stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 }
